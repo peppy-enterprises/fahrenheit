c0e713aad38a6f07486743a1c73e47270aef3d67
Fran Kelava <fran.kelava@outlook.com>
2023-05-21 02:10:50 +0200

    Development snapshot 21/5/23 02:07.

--- Fahrenheit.CT2H/ct2csmain.cs
+++ Fahrenheit.CT2H/ct2csmain.cs
@@ -5,41 +5,46 @@
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
+using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 
-namespace Fahrenheit.CT2H;
+namespace Fahrenheit.CT2CS;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Option<string> optDefaultNamespace = new Option<string>("--ns", "Set the namespace of the resulting C# file.");
-        Option<string> optFilePath         = new Option<string>("--src", "Set the path to the source file.");
+        Option<string> optDefNs    = new Option<string>("--ns", "Set the namespace of the resulting C# file.");
+        Option<string> optFilePath = new Option<string>("--src", "Set the path to the source file.");
+        Option<string> optDestPath = new Option<string>("--dest", "Set the folder where the C# file should be written.");
 
-        optDefaultNamespace.IsRequired = true;
-        optFilePath.IsRequired         = true;
+        optDefNs.IsRequired    = true;
+        optFilePath.IsRequired = true;
+        optDestPath.IsRequired = true;
 
         RootCommand rootCmd = new RootCommand("Process a cheat table and create a C# code file.")
         {
-            optDefaultNamespace, 
-            optFilePath
+            optDefNs, 
+            optFilePath,
+            optDestPath
         };
 
-        rootCmd.SetHandler(CT2HMain, new CT2HArgsBinder(
-            optDefaultNamespace, 
-            optFilePath));
+        rootCmd.SetHandler(CT2CSMain, new CT2CSArgsBinder(
+            optDefNs, 
+            optFilePath,
+            optDestPath));
 
         rootCmd.Invoke(args);
         return;
     }
 
-    static void CT2HMain(FhCT2HArgs config)
+    static void CT2CSMain(FhCT2CSArgs config)
     {
-        CT2HConfig.CLIRead(config);
+        CT2CSConfig.CLIRead(config);
 
-        using (XmlReader xmlr = XmlReader.Create(config.FilePath))
+        using (XmlReader xmlr = XmlReader.Create(config.SrcPath))
         {
             xmlr.ReadToDescendant("CheatEntry");
 
@@ -63,18 +68,18 @@ internal class Program
                     continue;
 
                 StringBuilder sb = new StringBuilder();
-                string        fn = $"{node.Name}-{Guid.NewGuid()}.cs";
+                string        fn = Path.Join(CT2CSConfig.DestPath, $"{node.Name}-{Guid.NewGuid()}.g.cs");
 
                 if (!stn.TryEmitStruct(sb))
                     throw new Exception("E_EMIT_FAULT");
 
-                //using (FileStream fs = File.Open(fn, FileMode.CreateNew))
-                //{
-                //    using (StreamWriter sw = new StreamWriter(fs))
-                //    {
-                //        sw.Write(sb.ToString());
-                //    }
-                //}
+                using (FileStream fs = File.Open(fn, FileMode.CreateNew))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.Write(sb.ToString());
+                    }
+                }
 
                 Console.WriteLine(sb.ToString());
                 Console.WriteLine($"Struct {stn.Name}: Output is at {fn}.");
---
+++ Fahrenheit.CT2H/ct2csopts.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Binding;
+using System.IO;
+
+namespace Fahrenheit.CT2CS;
+
+internal static class CT2CSConfig
+{
+    public static string DefaultNamespace = string.Empty;
+    public static string SrcPath          = string.Empty;
+    public static string DestPath         = string.Empty;
+
+    public static void CLIRead(FhCT2CSArgs args)
+    {
+        DefaultNamespace = args.DefaultNamespace;
+        SrcPath          = args.SrcPath;
+        DestPath         = Directory.Exists(args.DestPath) ? args.DestPath : throw new Exception("E_INVALID_DEST_DIR");
+    }
+}
+
+internal sealed record FhCT2CSArgs(string DefaultNamespace,
+                                   string SrcPath,
+                                   string DestPath);
+
+internal class CT2CSArgsBinder : BinderBase<FhCT2CSArgs>
+{
+    private readonly Option<string> _optDefNs;
+    private readonly Option<string> _optSrcPath;
+    private readonly Option<string> _optDestPath;
+
+    public CT2CSArgsBinder(Option<string> optDefNs, 
+                           Option<string> optFilePath,
+                           Option<string> optDestPath)
+    {
+        _optDefNs    = optDefNs;
+        _optSrcPath  = optFilePath;
+        _optDestPath = optDestPath;
+    }
+
+    protected override FhCT2CSArgs GetBoundValue(BindingContext bindingContext)
+    {
+        string defNs    = bindingContext.ParseResult.GetValueForOption(_optDefNs) ?? throw new Exception("E_CLI_ARG_NULL");
+        string srcPath  = bindingContext.ParseResult.GetValueForOption(_optSrcPath) ?? throw new Exception("E_CLI_ARG_NULL");
+        string destPath = bindingContext.ParseResult.GetValueForOption(_optDestPath) ?? throw new Exception("E_CLI_ARG_NULL");
+
+        return new FhCT2CSArgs(defNs, srcPath, destPath);
+    }
+}
