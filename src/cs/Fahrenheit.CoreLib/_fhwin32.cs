fa22678224dcbc488a90e85ddcf84880f0041ad9
Fran Kelava <fran.kelava@outlook.com>
2023-06-06 21:55:40 +0200

    Development snapshot 6/6/23 21:55.

--- src/cs/Fahrenheit.CoreLib/_fhwin32.cs
+++ src/cs/Fahrenheit.CoreLib/_fhwin32.cs
@@ -1,20 +1,24 @@
-﻿using System;
+﻿namespace Fahrenheit.CoreLib;
 
-namespace Fahrenheit.CoreLib;
+/* [fkelava 6/6/23 21:29]
+ * P/Invokes cannot be declared here because of https://github.com/dotnet/runtime/issues/87188.
+ * 
+ * Until this is fixed, P/Invokes must be declared in the hook assembly itself.
+ */
 
 public static partial class FhWin32
 {
-    [LibraryImport("msvcrt.dll", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(System.Runtime.InteropServices.Marshalling.AnsiStringMarshaller))]
-    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
-    public static partial int _vscprintf(string format, IntPtr ptr);
+    //[LibraryImport("msvcrt.dll", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(System.Runtime.InteropServices.Marshalling.AnsiStringMarshaller))]
+    //[UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    //public static partial int _vscprintf(string format, IntPtr ptr);
 
-    [LibraryImport("msvcrt.dll", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(System.Runtime.InteropServices.Marshalling.AnsiStringMarshaller))]
-    [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
-    public static partial int vsprintf(IntPtr buffer, string format, IntPtr args);
+    //[LibraryImport("msvcrt.dll", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(System.Runtime.InteropServices.Marshalling.AnsiStringMarshaller))]
+    //[UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+    //public static partial int vsprintf(IntPtr buffer, string format, IntPtr args);
 
-    [DllImport("msvcrt.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int sprintf(IntPtr buffer, string format, __arglist);
+    //[DllImport("msvcrt.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    //public static extern int sprintf(IntPtr buffer, string format, __arglist);
 
-    [DllImport("msvcrt.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-    public static extern int _scprintf(string format, __arglist);
+    //[DllImport("msvcrt.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    //public static extern int _scprintf(string format, __arglist);
 }
--- src/cs/Fahrenheit.HookLib/printfh.cs
+++ src/cs/Fahrenheit.HookLib/printfh.cs
@@ -6,63 +6,101 @@ using Fahrenheit.CoreLib;
 
 namespace Fahrenheit.HookLib;
 
-public static class Hook
+/* [fkelava 6/6/23 21:29]
+ * P/Invokes must be declared here because of https://github.com/dotnet/runtime/issues/87188.
+ * 
+ * Until this is fixed, P/Invokes cannot be declared in fhcorlib and you must therefore carry your own P/Invokes.
+ * 
+ * Always declare your P/Invokes internal if bundling them in the hook library as such!
+ */
+
+internal static class FhPInvoke
 {
-    private static readonly object       _hookLock;
+#pragma warning disable SYSLIB1054 // Specifically disabled because LibraryImportAttribute cannot handle __arglist, presumably.
+    [DllImport("msvcrt.dll", CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int sprintf(IntPtr buffer, string format, __arglist);
+
+    [DllImport("msvcrt.dll", CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int _scprintf(string format, __arglist);
+#pragma warning restore SYSLIB1054
+}
+
+public static class FhHookDelegates
+{
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void PrintfVarargDelegate(string fmt, nint va0, nint va1, nint va2, nint va3, nint va4, nint va5, nint va6, nint va7, nint va8, nint va9, nint va10, nint va11, nint va12, nint va13, nint va14, nint va15);
+}
+
+public static class FhHooks
+{
     private static readonly StreamWriter _logFile;
 
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void CLRPrintfDelegate(
-        string fmt, 
-        nint   va0,
-        nint   va1, 
-        nint   va2, 
-        nint   va3, 
-        nint   va4, 
-        nint   va5,
-        nint   va6,
-        nint   va7,
-        nint   va8,
-        nint   va9,
-        nint   va10,
-        nint   va11,
-        nint   va12,
-        nint   va13,
-        nint   va14,
-        nint   va15
-        );
-
-    static Hook()
+    static FhHooks()
     {
-        _hookLock = new object();
-        _logFile  = new StreamWriter(File.Open("clrhost.log", FileMode.Append, FileAccess.Write, FileShare.Read));
+        _logFile = new StreamWriter(File.Open("dbgPrintf.log", FileMode.Append, FileAccess.Write, FileShare.Read));
     }
 
-    [FhHook(HookTarget.X, 0x22F6B0, typeof(CLRPrintfDelegate))]
+    [FhHook(HookTarget.X, 0x22F6B0, typeof(FhHookDelegates.PrintfVarargDelegate))]
     [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
-    public static void CLRPrintfHook(
-        string fmt,
-        nint   va0,
-        nint   va1,
-        nint   va2,
-        nint   va3,
-        nint   va4,
-        nint   va5,
-        nint   va6,
-        nint   va7,
-        nint   va8,
-        nint   va9,
-        nint   va10,
-        nint   va11,
-        nint   va12,
-        nint   va13,
-        nint   va14,
-        nint   va15
-        )
+    public static void CLRPrintfHookAnsi(string fmt, nint va0, nint va1, nint va2, nint va3, nint va4, nint va5, nint va6, nint va7, nint va8, nint va9, nint va10, nint va11, nint va12, nint va13, nint va14, nint va15)
     {
         int argc = 0;
 
         for (int i = 0; i < fmt.Length; i++) if (fmt[i] == '%') argc++;
-        FhLog.Log(LogLevel.Info, $"fmt: {fmt}");
+
+        int bl = argc switch
+        {
+            0  => FhPInvoke._scprintf(fmt, __arglist()),
+            1  => FhPInvoke._scprintf(fmt, __arglist(va0)),
+            2  => FhPInvoke._scprintf(fmt, __arglist(va0, va1)),
+            3  => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2)),
+            4  => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3)),
+            5  => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4)),
+            6  => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5)),
+            7  => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6)),
+            8  => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7)),
+            9  => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8)),
+            10 => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9)),
+            11 => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10)),
+            12 => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11)),
+            13 => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12)),
+            14 => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12, va13)),
+            15 => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12, va13, va14)),
+            16 => FhPInvoke._scprintf(fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12, va13, va14, va15)),
+            _  => throw new Exception("FH_E_PFHOOK_RAH_OVERREACH")
+        };
+
+        nint buf = Marshal.AllocHGlobal(bl + 1);
+
+        try
+        {
+            int rv = argc switch
+            {
+                0  => FhPInvoke.sprintf(buf, fmt, __arglist()),
+                1  => FhPInvoke.sprintf(buf, fmt, __arglist(va0)),
+                2  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1)),
+                3  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2)),
+                4  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3)),
+                5  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4)),
+                6  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5)),
+                7  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6)),
+                8  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7)),
+                9  => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8)),
+                10 => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9)),
+                11 => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10)),
+                12 => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11)),
+                13 => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12)),
+                14 => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12, va13)),
+                15 => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12, va13, va14)),
+                16 => FhPInvoke.sprintf(buf, fmt, __arglist(va0, va1, va2, va3, va4, va5, va6, va7, va8, va9, va10, va11, va12, va13, va14, va15)),
+                _  => throw new Exception("FH_E_PFHOOK_RAH_OVERREACH")
+            };
+
+            _logFile.Write(Marshal.PtrToStringAnsi(buf));
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(buf);
+        }
     }
 }
