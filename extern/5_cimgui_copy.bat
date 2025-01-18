7b42753e5db3da5f609a3a04a405ca2128a71d5f
Fran Kelava <fran.kelava@outlook.com>
2025-01-19 00:27:37 +0100

    Remove the bulk of `extern`.
    It is due to be migrated to a different repo,
    with Fahrenheit only consuming its raw artifacts.

--- extern/5_cimgui_copy.bat
+++
@@ -1,9 +0,0 @@
-:: Fahrenheit `extern` build, Stage 5 : cimgui copy
-:: 24/9/24 00:28 fkelava
-
-:: Prepares the JSON files generated in Step 3 and DLL generated in Step 4a/4b for C# binding generation.
-
-robocopy cimgui-bin fh-imgui-net\deps\cimgui\win-x86 cimgui.dll
-robocopy cimgui/generator/output fh-imgui-net\src\CodeGenerator\definitions\cimgui definitions.json
-robocopy cimgui/generator/output fh-imgui-net\src\CodeGenerator\definitions\cimgui impl_definitions.json
-robocopy cimgui/generator/output fh-imgui-net\src\CodeGenerator\definitions\cimgui structs_and_enums.json
--- extern/6_codegen_build.bat
+++
@@ -1,9 +0,0 @@
-:: Fahrenheit `extern` build, Stage 6 : ImGui.NET code generator build
-:: 24/9/24 00:28 fkelava
-
-@echo off 
-
-cd fh-imgui-net\src\CodeGenerator
-:: There is no point in building this in Release. Performance is irrelevant
-:: but the stack traces are way too useful to forgo.
-dotnet build CodeGenerator.csproj -c Debug
