10795c4f4df8112f1a67f982a9e0da13b03b756e
Fran Kelava <fran.kelava@outlook.com>
2025-02-12 00:51:11 +0100

    Merge EFL with Runtime.
    EFL is now a non-negotiably active part of Fahrenheit.
    It scans ``efl/x`` and ``efl/x2`` in every mod directory.

---
+++ src/runtime/efl.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+
+using static Fahrenheit.Core.Runtime.PInvoke;
+
+namespace Fahrenheit.Core.Runtime;
+
+public struct FhFfxFile {
+    public nint handle_os;
+    public nint handle_vbf;
+}
+
+[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+public unsafe delegate FhFfxFile* fiosOpen(nint path_ptr, bool read_only);
+
+public sealed record FhEFLModuleConfig : FhModuleConfig {
+    [JsonConstructor]
+    public FhEFLModuleConfig(string name) : base(name) { }
+
+    public override FhEFLModule SpawnModule() {
+        return new FhEFLModule(this);
+    }
+}
+
+public unsafe class FhEFLModule : FhModule {
+    private readonly FhEFLModuleConfig          _config;
+    private readonly Dictionary<string, string> _index;
+    private readonly FhMethodHandle<fiosOpen>   _h_fiosOpen;
+
+    public FhEFLModule(FhEFLModuleConfig cfg) : base(cfg) {
+        _config     = cfg;
+        _index      = [];
+        _h_fiosOpen = new(this, "FFX.exe", h_open, offset: 0x2798E0);
+    }
+
+    // the game uses a fixed stream prefix "../../../" - I don't see why ffgriever handled the other edge cases (yet)
+    private static string normalize_path(string path) {
+        string prefixless_path = path.Replace("../../../", "");
+
+        return OperatingSystem.IsWindows()
+            ? prefixless_path.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar)
+            : prefixless_path;
+    }
+
+    public void construct_index() {
+        Stopwatch index_swatch     = Stopwatch.StartNew();
+        string    data_subdir_name = FhGlobal.game_type switch {
+            FhGameType.FFX  => "efl/x",
+            FhGameType.FFX2 => "efl/x2",
+            _               => throw new Exception("FH_E_INVALID_GAME_TYPE"),
+        };
+
+        string efl_data_dir;
+        foreach (string module_directory in Directory.EnumerateDirectories(FhRuntimeConst.Modules.LinkPath)) {
+            efl_data_dir = normalize_path(Path.Join(module_directory, data_subdir_name));
+            if (!Directory.Exists(efl_data_dir)) continue;
+
+            foreach (string absolute_mod_file_path in Directory.GetFiles(efl_data_dir, "*.*", SearchOption.AllDirectories)) {
+                string nt_absolute_mod_file_path = @$"\\?\{absolute_mod_file_path}";
+                string relative_mod_file_path    = Path.GetRelativePath(efl_data_dir, absolute_mod_file_path);
+                string normalized_relative_path  = normalize_path(relative_mod_file_path);
+                string normalized_absolute_path  = normalize_path(OperatingSystem.IsWindows()
+                    ? nt_absolute_mod_file_path
+                    : absolute_mod_file_path);
+
+                if (!_index.TryAdd(
+                    key:   normalized_relative_path,
+                    value: normalized_absolute_path)) {
+                    FhLog.Warning($"{normalized_relative_path} was already loaded by a module higher in the load order; ignoring.");
+                }
+            }
+        }
+
+        index_swatch.Stop();
+        FhLog.Warning($"EFL indexing complete in {index_swatch.ElapsedMilliseconds} ms.");
+    }
+
+    public FhFfxFile* h_open(nint path_ptr, bool read_only) {
+        string     path            = Marshal.PtrToStringAnsi(path_ptr) ?? throw new Exception("FH_E_EFL_FIOS_OPEN_PATH_NUL");
+        string     normalized_path = normalize_path(path);
+        FhFfxFile* file            = _h_fiosOpen.orig_fptr.Invoke(path_ptr, read_only);
+
+        if (!_index.TryGetValue(normalized_path, out string? modded_path)) return file;
+
+        /* [fkelava 01/10/24 16:49]
+         * FFX.exe+208100 at +2081B9 onward:
+         * if (readOnly) { pvVar4 = CreateFileW(path, 1, 1, 0, 3, 0x08000000, 0); }
+         * else          { pvVar4 = CreateFileW(path, 2, 0, 0, 4, 0x08000000, 0); }
+         *
+         * No bookkeeping of the returned handle is necessary. The game closes it itself.
+         */
+
+        file->handle_vbf = 0;
+        file->handle_os  = CreateFileW(
+            lpFileName:            modded_path,
+            dwDesiredAccess:       read_only ? FILE_READ_DATA  : FILE_WRITE_DATA,
+            dwShareMode:           read_only ? FILE_SHARE_READ : 0U,
+            lpSecurityAttributes:  0,
+            dwCreationDisposition: read_only ? OPEN_EXISTING   : OPEN_ALWAYS,
+            dwFlagsAndAttributes:  FILE_FLAG_SEQUENTIAL_SCAN,
+            hTemplateFile:         0);
+
+        FhLog.Info($"{path} -> {modded_path}");
+        return file;
+    }
+
+    public override bool init() {
+        construct_index();
+        return _h_fiosOpen.hook();
+    }
+}
--- src/runtime/pinv.cs
+++ src/runtime/pinv.cs
@@ -3,7 +3,24 @@
 namespace Fahrenheit.Core.Runtime;
 
 internal static unsafe partial class PInvoke {
-    internal const int GWLP_WNDPROC = -4;
+    [LibraryImport("kernel32.dll", StringMarshalling = StringMarshalling.Utf16)]
+    internal static partial nint CreateFileW(
+        string lpFileName,
+        uint   dwDesiredAccess,
+        uint   dwShareMode,
+        nint   lpSecurityAttributes,
+        uint   dwCreationDisposition,
+        uint   dwFlagsAndAttributes,
+        nint   hTemplateFile);
+
+    // https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilew
+    internal const uint FILE_READ_DATA            = 1;
+    internal const uint FILE_WRITE_DATA           = 2;
+    internal const uint FILE_SHARE_READ           = 1;
+    internal const uint OPEN_EXISTING             = 3;
+    internal const uint OPEN_ALWAYS               = 4;
+    internal const uint FILE_FLAG_SEQUENTIAL_SCAN = 0x08000000;
+    internal const int  GWLP_WNDPROC              = -4;
 
     [LibraryImport("user32.dll")]
     internal static partial nint CallWindowProcW(
