333f8f5b5976f534d746c63eefc2a858fef1128d
Fran Kelava <fran.kelava@outlook.com>
2025-07-19 03:59:00 +0200

    Let JIT decide inlining; nix unused util method
    - We never actually tested whether JIT inlines these
    or how profitable that inline necessarily is.

--- base/core/util.cs
+++ base/core/util.cs
@@ -12,13 +12,11 @@ public static unsafe class FhUtil {
         return rv;
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void cast_to_bytes<T>(in ReadOnlySpan<T> src, in Span<byte> dest, out int bytesWritten) where T : struct {
         bytesWritten = Unsafe.SizeOf<T>() * src.Length;
         MemoryMarshal.AsBytes(src).CopyTo(dest);
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void cast_from_bytes<T>(in ReadOnlySpan<byte> src, in Span<T> dest, int srcLen, out int count) where T : struct {
         count = srcLen / Unsafe.SizeOf<T>();
         MemoryMarshal.Cast<byte, T>(src).CopyTo(dest);
@@ -27,7 +25,6 @@ public static unsafe class FhUtil {
     /// <summary>
     ///     Generic bitwise NOT.
     /// </summary>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T not_with_cast<T>(this T x) where T : IBinaryInteger<T> {
         unchecked { return ~x; }
     }
@@ -36,7 +33,6 @@ public static unsafe class FhUtil {
     /// <returns>
     ///     Whether bit at <paramref name="offset"/> into <paramref name="bitField"/> is set.
     /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe bool get_bit<T>(this T bitField, int offset) where T : unmanaged, IBinaryInteger<T> {
         if (offset < 0 || offset >= sizeof(T) * 8) throw new ArgumentOutOfRangeException(nameof(offset));
 
@@ -48,7 +44,6 @@ public static unsafe class FhUtil {
     /// <returns>
     ///     The value of type <typeparamref name="T"/> made from <paramref name="len"/> bits <paramref name="offset"/> into <paramref name="bitField"/>.
     /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe T get_bits<T>(this T bitField, int offset, int len) where T : unmanaged, IBinaryInteger<T> {
         if (offset <  0 || offset >=  sizeof(T) * 8)           throw new ArgumentOutOfRangeException(nameof(offset));
         if (len    <= 0 || len    >  (sizeof(T) * 8) - offset) throw new ArgumentOutOfRangeException(nameof(len));
@@ -61,7 +56,6 @@ public static unsafe class FhUtil {
     /// </summary>
     /// <param name="offset">A 0-based offset into the <paramref name="bitField"/>.</param>
     /// <param name="value">Whether the bit should be set to <c>1</c> if <c>true</c> or <c>0</c> if <c>false</c>.</param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe void set_bit<T>(this ref T bitField, int offset, bool value) where T : unmanaged, IBinaryInteger<T> {
         if (offset < 0 || offset >= sizeof(T) * 8) throw new ArgumentOutOfRangeException(nameof(offset));
 
@@ -75,7 +69,6 @@ public static unsafe class FhUtil {
     /// <param name="offset">A 0-based offset into the <paramref name="bitField"/>.</param>
     /// <param name="len">The amount of bits to write.</param>
     /// <param name="value">The value to set the bits to. Only the first <paramref name="len"/> bits matter.</param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static unsafe void set_bits<T>(this ref T bitField, int offset, int len, T value) where T : unmanaged, IBinaryInteger<T> {
         if (offset < 0  || offset >=  sizeof(T) * 8)           throw new ArgumentOutOfRangeException(nameof(offset));
         if (len    <= 0 || len    >  (sizeof(T) * 8) - offset) throw new ArgumentOutOfRangeException(nameof(len));
@@ -96,7 +89,6 @@ public static unsafe class FhUtil {
     ///     Packs a byte span into an <see cref="ulong"/>, little-endian.
     ///     Undefined on inputs over 8 bytes in length.
     /// </summary>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ulong pack_bytes_le(this ReadOnlySpan<byte> bytes) {
         ulong le = 0;
 
@@ -110,7 +102,6 @@ public static unsafe class FhUtil {
     ///     Packs a byte span into an <see cref="ulong"/>, big-endian.
     ///     Undefined on inputs over 8 bytes in length.
     /// </summary>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ulong pack_bytes_be(this ReadOnlySpan<byte> bytes) {
         ulong be = 0;
 
@@ -149,17 +140,6 @@ public static unsafe class FhUtil {
             reader.Read();
         }
         reader.Read();
-    }
-
-    internal static T deserialize_and_advance<T>(this ref Utf8JsonReader reader, string key) {
-        if (reader.GetString() != key)
-            throw new JsonException($"Expected {key}, got {reader.GetString()}.");
-
-        reader.Read();
-        T tval = JsonSerializer.Deserialize<T>(ref reader, InternalJsonOpts) ?? throw new JsonException();
-        reader.Read();
-
-        return tval;
     }
 
     public static TDelegate get_fptr<TDelegate>(nint funcAddress) {
