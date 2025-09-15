28b6c1e7c2559c58378c9355646fabef3fad0956
Fran Kelava <fran.kelava@outlook.com>
2025-09-15 16:41:08 +0200

    Fuse 'bootstrapper.cs' and 'loader.cs'
    Their responsibilities are analogous and the
    combined file is highly legible.

--- base/core/bootloader.cs
+++ base/core/bootloader.cs
@@ -1,6 +1,22 @@
 ﻿namespace Fahrenheit.Core;
 
 /// <summary>
+///     Contains Fahrenheit boot logic. When the bootstrapper completes, game execution commences.
+/// </summary>
+public static class FhBootstrapper {
+    /* [fkelava 25/4/24 18:47]
+     * This class and method are referenced by Stage1. Updating or renaming either requires a Stage1 update.
+     */
+    [UnmanagedCallersOnly]
+    public static void bootstrap() {
+        FhApi.ModController = new(FhInternal.Loader.load_mods());
+
+        FhApi.LocalizationManager.construct_localization_map();
+        FhApi.ModController.initialize_mods();
+    }
+}
+
+/// <summary>
 ///     Instructs the <see cref="FhLoader"/> that this module type should be instantiated,
 ///     providing the <paramref name="supported_game_type"/> matches the currently executing game.
 /// </summary>
