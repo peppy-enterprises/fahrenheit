b6b55bfc86580c73d8b9f5cb81bcaf4729fe5d81
Fran Kelava <fran.kelava@outlook.com>
2025-09-23 21:38:16 +0200

    SGEN: add /ffx2/master/jppc/battle/kernel/ply_save

---
+++ core/ffx2/PlySaveId.cs
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+// Ported from ffx2/master/jppc/battle/kernel/ply_save.h in the Switch release of FFX/X-2 HD
+namespace Fahrenheit.Core.FFX2;
+
+public static partial class PlySaveId
+{
+    [NativeTypeName("#define PC_Yuna 0x00000")]
+    public const int PC_Yuna = 0x00000;
+
+    [NativeTypeName("#define PC_Rikku 0x00001")]
+    public const int PC_Rikku = 0x00001;
+
+    [NativeTypeName("#define PC_Pain 0x00002")]
+    public const int PC_Pain = 0x00002;
+
+    [NativeTypeName("#define PC_Yuna_Child_1 0x00003")]
+    public const int PC_Yuna_Child_1 = 0x00003;
+
+    [NativeTypeName("#define PC_Yuna_Child_2 0x00004")]
+    public const int PC_Yuna_Child_2 = 0x00004;
+
+    [NativeTypeName("#define PC_Rikku_Child_1 0x00005")]
+    public const int PC_Rikku_Child_1 = 0x00005;
+
+    [NativeTypeName("#define PC_Rikku_Child_2 0x00006")]
+    public const int PC_Rikku_Child_2 = 0x00006;
+
+    [NativeTypeName("#define PC_Pain_Child_1 0x00007")]
+    public const int PC_Pain_Child_1 = 0x00007;
+
+    [NativeTypeName("#define PC_Pain_Child_2 0x00008")]
+    public const int PC_Pain_Child_2 = 0x00008;
+
+    [NativeTypeName("#define PC_dog 0x00009")]
+    public const int PC_dog = 0x00009;
+
+    [NativeTypeName("#define PC_monkey 0x0000a")]
+    public const int PC_monkey = 0x0000a;
+
+    [NativeTypeName("#define PC_bird 0x0000b")]
+    public const int PC_bird = 0x0000b;
+
+    [NativeTypeName("#define PC_dummy 0x0000c")]
+    public const int PC_dummy = 0x0000c;
+}
--- core/ffx2/plysave.cs
+++ core/ffx2/plysave.cs
@@ -1,56 +1,117 @@
-﻿/* [fkelava 16/5/23 10:46]
- * source: Steam ver.
- * header: header_btl_ply_save
- *
- * /ffx_ps2/ffx2/master/jppc/battle/kernel/ply_save.h
- */
+// SPDX-License-Identifier: MIT
 
+// Ported from ffx2/master/jppc/battle/kernel/ply_save.h in the Switch release of FFX/X-2 HD
 namespace Fahrenheit.Core.FFX2;
 
-[StructLayout(LayoutKind.Explicit, Size = 0x80)]
-public unsafe struct PlySave {
-    [FieldOffset(0x0)]  public       uint   name;
-    [FieldOffset(0x4)]  public       uint   hp_bonus;
-    [FieldOffset(0x8)]  public       uint   mp_bonus;
-    [FieldOffset(0xC)]  public       byte   str_bonus;
-    [FieldOffset(0xD)]  public       byte   vit_bonus;
-    [FieldOffset(0xE)]  public       byte   mag_bonus;
-    [FieldOffset(0xF)]  public       byte   spirit_bonus;
-    [FieldOffset(0x10)] public       byte   dex_bonus;
-    [FieldOffset(0x11)] public       byte   luck_bonus;
-    [FieldOffset(0x12)] public       byte   avoid_bonus;
-    [FieldOffset(0x13)] public       byte   hit_bonus;
-    [FieldOffset(0x14)] public       uint   exp;
-    [FieldOffset(0x18)] public       uint   next_exp;
-    [FieldOffset(0x1C)] public       uint   hp;
-    [FieldOffset(0x20)] public       uint   mp;
-    [FieldOffset(0x24)] public       uint   hp_max;
-    [FieldOffset(0x28)] public       uint   mp_max;
-    [FieldOffset(0x2C)] public       byte   party;
-    [FieldOffset(0x2D)] public       byte   str;
-    [FieldOffset(0x2E)] public       byte   vit;
-    [FieldOffset(0x2F)] public       byte   mag;
-    [FieldOffset(0x30)] public       byte   spirit;
-    [FieldOffset(0x31)] public       byte   dex;
-    [FieldOffset(0x32)] public       byte   hit;
-    [FieldOffset(0x33)] public       byte   avoid;
-    [FieldOffset(0x34)] public       byte   luck;
-    [FieldOffset(0x35)] public       byte   reserve;
-    [FieldOffset(0x36)] public       ushort job;
-    [FieldOffset(0x38)] public       ushort plate;
-    [FieldOffset(0x3A)] public fixed ushort accessory[2];
-    [FieldOffset(0x3E)] public       ushort command;
-    [FieldOffset(0x40)] public       uint   escape_count;
-    [FieldOffset(0x44)] public       uint   kill_count;
-    [FieldOffset(0x48)] public       uint   death_count;
-    [FieldOffset(0x4C)] public       int    status;
-    [FieldOffset(0x50)] public fixed ushort ability_type[3];
-    [FieldOffset(0x56)] public       ushort before_job;
+public unsafe partial struct PlySave
+{
+    [NativeTypeName("unsigned int")]
+    public uint name;
 
-    [FieldOffset(0x7A)] public       byte   size;
+    [NativeTypeName("unsigned int")]
+    public uint hp_bonus;
 
-    public bool join   { readonly get { return party.get_bit(0); } set { party.set_bit(0, value); } }
-    public bool left   { readonly get { return party.get_bit(1); } set { party.set_bit(1, value); } }
-    public bool fix    { readonly get { return party.get_bit(2); } set { party.set_bit(2, value); } }
-    public bool joined { readonly get { return party.get_bit(4); } set { party.set_bit(4, value); } }
+    [NativeTypeName("unsigned int")]
+    public uint mp_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte str_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte vit_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte mag_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte spirit_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte dex_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte luck_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte avoid_bonus;
+
+    [NativeTypeName("unsigned char")]
+    public byte hit_bonus;
+
+    [NativeTypeName("unsigned int")]
+    public uint exp;
+
+    [NativeTypeName("unsigned int")]
+    public uint next_exp;
+
+    [NativeTypeName("unsigned int")]
+    public uint hp;
+
+    [NativeTypeName("unsigned int")]
+    public uint mp;
+
+    [NativeTypeName("unsigned int")]
+    public uint hp_max;
+
+    [NativeTypeName("unsigned int")]
+    public uint mp_max;
+
+    [NativeTypeName("unsigned char")]
+    public byte party;
+
+    [NativeTypeName("unsigned char")]
+    public byte str;
+
+    [NativeTypeName("unsigned char")]
+    public byte vit;
+
+    [NativeTypeName("unsigned char")]
+    public byte mag;
+
+    [NativeTypeName("unsigned char")]
+    public byte spirit;
+
+    [NativeTypeName("unsigned char")]
+    public byte dex;
+
+    [NativeTypeName("unsigned char")]
+    public byte hit;
+
+    [NativeTypeName("unsigned char")]
+    public byte avoid;
+
+    [NativeTypeName("unsigned char")]
+    public byte luck;
+
+    [NativeTypeName("unsigned char")]
+    public byte reserve;
+
+    [NativeTypeName("unsigned short")]
+    public ushort job;
+
+    [NativeTypeName("unsigned short")]
+    public ushort plate;
+
+    [NativeTypeName("unsigned short[2]")]
+    public fixed ushort accessory[2];
+
+    [NativeTypeName("unsigned short")]
+    public ushort command;
+
+    [NativeTypeName("unsigned int")]
+    public uint escape_count;
+
+    [NativeTypeName("unsigned int")]
+    public uint kill_count;
+
+    [NativeTypeName("unsigned int")]
+    public uint death_count;
+
+    public int status;
+
+    [NativeTypeName("unsigned short[3]")]
+    public fixed ushort ability_type[3];
+
+    [NativeTypeName("unsigned short")]
+    public ushort before_job;
 }
