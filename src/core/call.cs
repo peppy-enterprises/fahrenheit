dfe1ca969a60d5ab660a64332a890449133c02a7
Fran Kelava <fran.kelava@outlook.com>
2026-05-12 20:52:45 +0200

    add manual call sinks

---
+++ src/core/call.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-License-Identifier: MIT
+
+/* [fkelava 26/04/26 15:26]
+ * Unlike `call.g.cs`, which contains source-generated delegates with no guarantee of accuracy,
+ * this file contains manually annotated calls with proper Fahrenheit types that are vetted for functionality.
+ *
+ * This file contains calls which are analogous between both games. Unlike the per-game `call.cs` files,
+ * functions included here are expected to also provide a 'select' helper for automatic .
+ */
+
+namespace Fahrenheit;
+
+public static unsafe partial class FhCall { }
---
+++ src/core/ffx2/call.cs
@@ -0,0 +1,12 @@
+﻿// SPDX-License-Identifier: MIT
+
+/* [fkelava 26/04/26 15:26]
+ * Unlike `call.g.cs`, which contains source-generated delegates with no guarantee of accuracy,
+ * this file contains manually annotated calls with proper Fahrenheit types that are vetted for functionality.
+ *
+ * This file is for calls which are exclusive to FF X-2/LM and not shared with X.
+ */
+
+namespace Fahrenheit.FFX2;
+
+public static unsafe partial class FhCall { }
