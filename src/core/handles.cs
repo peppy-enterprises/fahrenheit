077e615fad4cb78a0a750ae590108fa3eeb45158
Fran Kelava <fran.kelava@outlook.com>
2026-07-09 22:07:41 +0200

    review

--- src/core/handles.cs
+++ src/core/handles.cs
@@ -223,10 +223,10 @@ internal sealed class FhMethodContext {
 /// </summary>
 internal sealed class FhMethodTable {
 
-    private readonly static Dictionary<nint,     Delegate>        _s_fnptrs  = []; // Any function -> Cached delegate
-    private readonly static Dictionary<nint,     FhMethodContext> _s_methods = []; // Original     -> All hooks (for keep-alive)
-    private readonly static Dictionary<nint,     nint>            _s_insert  = []; // Original     -> Insertion address for next hook
-    private readonly static Dictionary<Delegate, nint>            _s_chain   = []; // Hook         -> Next function in chain
+    private readonly static Dictionary<nint,     Delegate>        _fnptrs  = []; // Any function -> Cached delegate
+    private readonly static Dictionary<nint,     FhMethodContext> _methods = []; // Original     -> All hooks (for keep-alive)
+    private readonly static Dictionary<nint,     nint>            _insert  = []; // Original     -> Insertion address for next hook
+    private readonly static Dictionary<Delegate, nint>            _chain   = []; // Hook         -> Next function in chain
 
     private          int  _lock_commit = 0;
     private readonly Lock _lock_chains = new Lock();
@@ -236,17 +236,17 @@ internal sealed class FhMethodTable {
     ///     at <paramref name="ptr_target"/>, or returns the cached one if it already exists.
     /// </summary>
     public T get_fnptr<T>(nint ptr_target) where T : Delegate {
-        if (_s_fnptrs.TryGetValue(ptr_target, out Delegate? fnptr) && fnptr is T t_fnptr)
+        if (_fnptrs.TryGetValue(ptr_target, out Delegate? fnptr) && fnptr is T t_fnptr)
             return t_fnptr;
 
         t_fnptr = Marshal.GetDelegateForFunctionPointer<T>(ptr_target);
-        _s_fnptrs[ptr_target] = t_fnptr;
+        _fnptrs[ptr_target] = t_fnptr;
         return t_fnptr;
     }
 
     /* [fkelava 04/06/26 23:28]
-     * Locking should not be required because _s_insert is only manipulated
-     * in a function under lock, and chain_from() which reads _s_chain is only
+     * Locking should not be required because _insert is only manipulated
+     * in a function under lock, and chain_from() which reads _chain is only
      * valid in contexts where no further hooks may be inserted.
      */
 
@@ -255,7 +255,7 @@ internal sealed class FhMethodTable {
     ///     the next function in the chain must be inserted.
     /// </summary>
     public nint get_ptr_insert(nint ptr_target) {
-        return _s_insert.TryGetValue(ptr_target, out nint ptr_insert)
+        return _insert.TryGetValue(ptr_target, out nint ptr_insert)
             ? ptr_insert
             : ptr_target;
     }
@@ -264,7 +264,7 @@ internal sealed class FhMethodTable {
     ///     For a given <paramref name="hook"/>, obtains the next link in its hook chain (if any exists).
     /// </summary>
     public T? get_fnptr_chain<T>(T hook) where T : Delegate {
-        return _s_chain.TryGetValue(hook, out nint ptr_chain)
+        return _chain.TryGetValue(hook, out nint ptr_chain)
             ? get_fnptr<T>(ptr_chain)
             : null;
     }
@@ -326,8 +326,8 @@ internal sealed class FhMethodTable {
             return false;
         }
 
-        _s_insert[ptr_target] = ppOriginal;
-        _s_chain [hook]       = ppOriginal;
+        _insert[ptr_target] = ppOriginal;
+        _chain [hook]       = ppOriginal;
 
         FhInternal.Log.Info($"(0x{ptr_target:X}) -> {hook.Method.Name}");
         return true;
@@ -336,7 +336,7 @@ internal sealed class FhMethodTable {
     /// <inheritdoc cref="fnptr_chain_insert{T}(nint, T)" />
     public bool fnptr_chain_add<T>(nint ptr_target, FhHookContext hook) where T : Delegate {
         lock (_lock_chains) {
-            if (_s_methods.TryGetValue(ptr_target, out FhMethodContext? target)) {
+            if (_methods.TryGetValue(ptr_target, out FhMethodContext? target)) {
                 if (target.tainted) {
                     FhInternal.Log.Error($"(0x{ptr_target:X}) - rejected late insertion of {hook.fnptr.Method.Name}");
                     return false;
@@ -349,7 +349,7 @@ internal sealed class FhMethodTable {
             target = new();
             target.stack.Push(hook);
 
-            _s_methods[ptr_target] = target;
+            _methods[ptr_target] = target;
             return Interlocked.CompareExchange(ref _lock_commit, 0, 0) == 0 || fnptr_chain_insert(ptr_target, hook.fnptr);
         }
     }
@@ -363,7 +363,7 @@ internal sealed class FhMethodTable {
             if (Interlocked.CompareExchange(ref _lock_commit, 1, 0) == 1)
                 return true; // reject repeat calls
 
-            foreach ((nint ptr_target, FhMethodContext target) in _s_methods) {
+            foreach ((nint ptr_target, FhMethodContext target) in _methods) {
                 Stack<FhHookContext> target_stack = target.stack;
 
                 foreach (FhHookContext hook in target_stack) {
