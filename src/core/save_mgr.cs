d4941c992b861a386f44f7e26c483e9dae593188
evelyntsmg <evelyntsmg@proton.me>
2026-07-21 21:35:03 +0200

    Add new methods for slot counts except autosave

--- src/core/save_mgr.cs
+++ src/core/save_mgr.cs
@@ -23,6 +23,7 @@ internal sealed class FhSaves {
     private readonly int[]                          _sm_active_set_slots;
     private readonly int[]                          _sm_active_set_saves;
     private          int                            _sm_active_set_count;
+    private          bool                           _sm_autosave_exists;
     private readonly string                         _sm_path_base;
     private readonly string                         _sm_path_default_set;
     private readonly FhSaveDisplayData[]            _sm_display_data;
@@ -126,6 +127,10 @@ internal sealed class FhSaves {
 
             _ = Encoding.UTF8.GetBytes($"{slot}\0", _sm_display_data[slot].slot);
             _ = Encoding.UTF8.GetBytes($"{save_file.LastWriteTimeUtc:yyyy/MM/dd HH:mm:ss}\0", _sm_display_data[slot].create_time);
+
+            if (slot == 0) {
+                _sm_autosave_exists = true;
+            }
 
             _sm_active_set_slots[slot]                   = 1;
             _sm_active_set_saves[_sm_active_set_count++] = slot;
@@ -233,14 +238,28 @@ internal sealed class FhSaves {
     ///     Get the number of used up slots in the current set.
     /// </summary>
     internal int get_slots_used() {
-        return _sm_active_set_count - 1; // Do not count the auto-save
+        return _sm_active_set_count;
+    }
+
+    /// <summary>
+    ///     Get the number of used up slots in the current set, not counting the auto-save slot.
+    /// </summary>
+    internal int get_slots_used_user() {
+        return _sm_active_set_count - (_sm_autosave_exists ? 1 : 0);
     }
 
     /// <summary>
     ///     Get the total number of slots in the current set.
     /// </summary>
     internal int get_slots_total() {
-        return _sm_set_size - 1; // Do not count the auto-save
+        return _sm_set_size;
+    }
+
+    /// <summary>
+    ///     Get the total number of slots in the current set, not counting the auto-save slot.
+    /// </summary>
+    internal int get_slots_total_user() {
+        return _sm_set_size - 1;
     }
 
     /// <summary>
--- src/runtime/save_ui.cs
+++ src/runtime/save_ui.cs
@@ -83,8 +83,7 @@ public sealed class FhSaveUiModule : FhModule {
 
         ReadOnlySpan<FhSaveDisplayData> display_data = FhInternal.Saves.get_display_data();
 
-        // get_slots_total() does not count the auto-save by default, so we have to add 1
-        for (; slot < FhInternal.Saves.get_slots_total() + 1; slot++) {
+        for (; slot < FhInternal.Saves.get_slots_total(); slot++) {
             if (display_data[slot].valid) {
                 ui_savefile(slot, display_data[slot]);
             }
@@ -109,7 +108,7 @@ public sealed class FhSaveUiModule : FhModule {
         }
 
         string active_set      = FhInternal.Saves.get_active_set();
-        string slots_text      = $"({FhInternal.Saves.get_slots_used()}/{FhInternal.Saves.get_slots_total()})";
+        string slots_text      = $"({FhInternal.Saves.get_slots_used_user()}/{FhInternal.Saves.get_slots_total_user()})";
         string active_set_text = $"{active_set} {slots_text}";
 
         float width_window = ImGui.GetWindowSize().X;
