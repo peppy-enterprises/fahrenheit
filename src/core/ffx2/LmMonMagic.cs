b8cdd125aaf96e8a53cc1b75ed61bf79468cb39d
Fran Kelava <fran.kelava@outlook.com>
2026-08-02 09:41:56 +0200

    generate `lm_command`, `lm_monmagic`, `lm_monster`

---
+++ src/core/ffx2/LmMonMagic.cs
@@ -0,0 +1,173 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// This file is part of Fahrenheit, © 2023-2026 The Fahrenheit contributors.
+// It is licensed to you under the GNU Lesser General Public License, version 3.0 or later. See COPYING, COPYING.LESSER.
+
+// ffx_ps2/ffx2/master/jppc/lastmiss/kernel/lm_monmagic.h
+// Steam release of FFX/X-2 HD
+
+namespace Fahrenheit.FFX2;
+
+public partial struct LmMonMagic
+{
+    [NativeTypeName("unsigned int")]
+    public uint name;
+
+    [NativeTypeName("unsigned char")]
+    public byte name_yn;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy1;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy2;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy3;
+
+    [NativeTypeName("unsigned int")]
+    public uint help;
+
+    [NativeTypeName("unsigned char")]
+    public byte help_yn;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy4;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy5;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy6;
+
+    [NativeTypeName("unsigned int")]
+    public uint information;
+
+    [NativeTypeName("unsigned short")]
+    public ushort personal_job;
+
+    [NativeTypeName("unsigned char")]
+    public byte char_job;
+
+    [NativeTypeName("unsigned char")]
+    public byte short_dist;
+
+    [NativeTypeName("unsigned char")]
+    public byte shot_range;
+
+    [NativeTypeName("unsigned char")]
+    public byte long_dist;
+
+    [NativeTypeName("unsigned char")]
+    public byte long_range;
+
+    [NativeTypeName("unsigned char")]
+    public byte stairchk_yn;
+
+    [NativeTypeName("unsigned char")]
+    public byte cursol_dist_yn;
+
+    [NativeTypeName("unsigned char")]
+    public byte cursol_cat;
+
+    [NativeTypeName("unsigned char")]
+    public byte target_pos;
+
+    [NativeTypeName("unsigned char")]
+    public byte cmdend;
+
+    [NativeTypeName("unsigned char")]
+    public byte cmdend_menu;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy7;
+
+    public short effect_no;
+
+    [NativeTypeName("unsigned char")]
+    public byte read_motion;
+
+    [NativeTypeName("unsigned char")]
+    public byte read_effect;
+
+    [NativeTypeName("unsigned char")]
+    public byte motion;
+
+    [NativeTypeName("unsigned char")]
+    public byte trun_change;
+
+    [NativeTypeName("unsigned char")]
+    public byte steal_st;
+
+    [NativeTypeName("unsigned char")]
+    public byte steal_hit;
+
+    [NativeTypeName("unsigned char")]
+    public byte use_mp;
+
+    [NativeTypeName("unsigned char")]
+    public byte use_hp;
+
+    [NativeTypeName("unsigned char")]
+    public byte target_param;
+
+    [NativeTypeName("unsigned char")]
+    public byte retdmg_motion;
+
+    [NativeTypeName("unsigned char")]
+    public byte critical;
+
+    [NativeTypeName("unsigned char")]
+    public byte calc_id;
+
+    [NativeTypeName("unsigned short")]
+    public ushort calc_no;
+
+    [NativeTypeName("unsigned short")]
+    public ushort atk_no;
+
+    [NativeTypeName("unsigned short")]
+    public ushort target_item_category;
+
+    [NativeTypeName("unsigned char")]
+    public byte category_abilty;
+
+    [NativeTypeName("unsigned char")]
+    public byte category_dmg_ret;
+
+    [NativeTypeName("unsigned char")]
+    public byte hit;
+
+    [NativeTypeName("unsigned char")]
+    public byte dark_hit;
+
+    [NativeTypeName("unsigned char")]
+    public byte hit_calc_id;
+
+    [NativeTypeName("unsigned char")]
+    public byte dummy8;
+
+    [NativeTypeName("unsigned short")]
+    public ushort blue_magic;
+
+    [NativeTypeName("unsigned char")]
+    public byte attribute_atk;
+
+    [NativeTypeName("unsigned char")]
+    public byte conf_use;
+
+    [NativeTypeName("unsigned char")]
+    public byte jibaku;
+
+    [NativeTypeName("unsigned char")]
+    public byte status_chg_target;
+
+    [NativeTypeName("unsigned char")]
+    public byte status_chg;
+
+    [NativeTypeName("unsigned char")]
+    public byte status_onoff;
+
+    [NativeTypeName("unsigned char")]
+    public byte status_hit;
+}
---
+++ src/core/ffx2/ids/LmMonmagicId.cs
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// This file is part of Fahrenheit, © 2023-2026 The Fahrenheit contributors.
+// It is licensed to you under the GNU Lesser General Public License, version 3.0 or later. See COPYING, COPYING.LESSER.
+
+// ffx_ps2/ffx2/master/jppc/lastmiss/kernel/lm_monmagic.h
+// Steam release of FFX/X-2 HD
+
+namespace Fahrenheit.FFX2.Ids;
+
+public static partial class LmMonmagicId
+{
+    [NativeTypeName("#define lm_mcom_atk_normal 0x04000")]
+    public const int lm_mcom_atk_normal = 0x04000;
+
+    [NativeTypeName("#define lm_mcom_atk_midrange 0x04001")]
+    public const int lm_mcom_atk_midrange = 0x04001;
+
+    [NativeTypeName("#define lm_mcom_atk_normal3x3 0x04002")]
+    public const int lm_mcom_atk_normal3x3 = 0x04002;
+
+    [NativeTypeName("#define lm_mcom_seed_cannon 0x04003")]
+    public const int lm_mcom_seed_cannon = 0x04003;
+
+    [NativeTypeName("#define lm_mcom_1000needle 0x04004")]
+    public const int lm_mcom_1000needle = 0x04004;
+
+    [NativeTypeName("#define lm_mcom_badsmell_breath 0x04005")]
+    public const int lm_mcom_badsmell_breath = 0x04005;
+
+    [NativeTypeName("#define lm_mcom_nucleus 0x04006")]
+    public const int lm_mcom_nucleus = 0x04006;
+
+    [NativeTypeName("#define lm_mcom_sunshine 0x04007")]
+    public const int lm_mcom_sunshine = 0x04007;
+
+    [NativeTypeName("#define lm_mcom_dead_count_alfa 0x04008")]
+    public const int lm_mcom_dead_count_alfa = 0x04008;
+
+    [NativeTypeName("#define lm_mcom_white_wind 0x04009")]
+    public const int lm_mcom_white_wind = 0x04009;
+
+    [NativeTypeName("#define lm_mcom_mighty_gard 0x0400a")]
+    public const int lm_mcom_mighty_gard = 0x0400a;
+
+    [NativeTypeName("#define lm_mcom_simor 0x0400b")]
+    public const int lm_mcom_simor = 0x0400b;
+
+    [NativeTypeName("#define lm_mcom_glare 0x0400c")]
+    public const int lm_mcom_glare = 0x0400c;
+
+    [NativeTypeName("#define lm_mcom_own_crash_bom 0x0400d")]
+    public const int lm_mcom_own_crash_bom = 0x0400d;
+
+    [NativeTypeName("#define lm_mcom_own_crash_pod 0x0400e")]
+    public const int lm_mcom_own_crash_pod = 0x0400e;
+
+    [NativeTypeName("#define lm_mcom_ice_breath 0x0400f")]
+    public const int lm_mcom_ice_breath = 0x0400f;
+
+    [NativeTypeName("#define lm_mcom_thunder_breath 0x04010")]
+    public const int lm_mcom_thunder_breath = 0x04010;
+
+    [NativeTypeName("#define lm_mcom_fire_breath1 0x04011")]
+    public const int lm_mcom_fire_breath1 = 0x04011;
+
+    [NativeTypeName("#define lm_mcom_fire_breath2 0x04012")]
+    public const int lm_mcom_fire_breath2 = 0x04012;
+
+    [NativeTypeName("#define lm_mcom_water_gun 0x04013")]
+    public const int lm_mcom_water_gun = 0x04013;
+
+    [NativeTypeName("#define lm_mcom_blaster 0x04014")]
+    public const int lm_mcom_blaster = 0x04014;
+
+    [NativeTypeName("#define lm_mcom_meteo 0x04015")]
+    public const int lm_mcom_meteo = 0x04015;
+
+    [NativeTypeName("#define lm_mcom_stone_glare 0x04016")]
+    public const int lm_mcom_stone_glare = 0x04016;
+
+    [NativeTypeName("#define lm_mcom_shock_wave 0x04017")]
+    public const int lm_mcom_shock_wave = 0x04017;
+
+    [NativeTypeName("#define lm_mcom_chest_beam 0x04018")]
+    public const int lm_mcom_chest_beam = 0x04018;
+
+    [NativeTypeName("#define lm_mcom_dead_count_normal 0x04019")]
+    public const int lm_mcom_dead_count_normal = 0x04019;
+
+    [NativeTypeName("#define lm_mcom_degeon 0x0401a")]
+    public const int lm_mcom_degeon = 0x0401a;
+
+    [NativeTypeName("#define lm_mcom_use_item 0x0401b")]
+    public const int lm_mcom_use_item = 0x0401b;
+
+    [NativeTypeName("#define lm_mcom_look_pl_01 0x0401c")]
+    public const int lm_mcom_look_pl_01 = 0x0401c;
+
+    [NativeTypeName("#define lm_mcom_look_pl_02 0x0401d")]
+    public const int lm_mcom_look_pl_02 = 0x0401d;
+
+    [NativeTypeName("#define lm_mcom_look_pl_03 0x0401e")]
+    public const int lm_mcom_look_pl_03 = 0x0401e;
+
+    [NativeTypeName("#define lm_mcom_look_pl_04 0x0401f")]
+    public const int lm_mcom_look_pl_04 = 0x0401f;
+
+    [NativeTypeName("#define lm_mcom_look_pl_05 0x04020")]
+    public const int lm_mcom_look_pl_05 = 0x04020;
+}
