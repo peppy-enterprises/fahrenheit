aab5dc951fe4690536b66711feeb8d6f8c4e9fe0
Fran Kelava <fran.kelava@outlook.com>
2026-08-04 00:08:13 +0200

    being silly has its downsides, like breaking things

--- src/core/imguihelp.cs
+++ src/core/imguihelp.cs
@@ -11,22 +11,11 @@ namespace Fahrenheit;
 public unsafe class FhImGuiHelper {
 
     // Fonts for standardized style across Fahrenheit
-    public readonly ImFontPtr FONT_DEFAULT;
-    public readonly ImFontPtr FONT_JP;
-    public readonly ImFontPtr FONT_KR;
-    public readonly ImFontPtr FONT_CH_S;
-    public readonly ImFontPtr FONT_CH_T;
-
-    public FhImGuiHelper() {
-        ImGuiIOPtr io        = ImGui.GetIO();
-        string     dir_fonts = Path.Join(FhEnvironment.Finder.Binaries.FullName, "resources", "fonts");
-
-        FONT_DEFAULT = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSans-Regular.ttf"),   20f);
-        FONT_JP      = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansJP-Regular.ttf"), 20f);
-        FONT_KR      = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansKR-Regular.ttf"), 20f);
-        FONT_CH_S    = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansSC-Regular.ttf"), 20f);
-        FONT_CH_T    = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansTC-Regular.ttf"), 20f);
-    }
+    public ImFontPtr FONT_DEFAULT;
+    public ImFontPtr FONT_JP;
+    public ImFontPtr FONT_KR;
+    public ImFontPtr FONT_CH_S;
+    public ImFontPtr FONT_CH_T;
 
     public void set_next_align(ReadOnlySpan<byte> label, float t, float padding = 0F) {
         float size      = ImGui.CalcTextSize(label).X + padding;
@@ -40,6 +29,17 @@ public unsafe class FhImGuiHelper {
 
     public enum FhImGuiThemes {
         CLASSIC_FF = 0,
+    }
+
+    private void _init_fonts() {
+        ImGuiIOPtr io        = ImGui.GetIO();
+        string     dir_fonts = Path.Join(FhEnvironment.Finder.Binaries.FullName, "resources", "fonts");
+
+        FONT_DEFAULT = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSans-Regular.ttf"),   20f);
+        FONT_JP      = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansJP-Regular.ttf"), 20f);
+        FONT_KR      = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansKR-Regular.ttf"), 20f);
+        FONT_CH_S    = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansSC-Regular.ttf"), 20f);
+        FONT_CH_T    = io.Fonts.AddFontFromFileTTF(Path.Join(dir_fonts, "NotoSansTC-Regular.ttf"), 20f);
     }
 
     private static void _init_style(FhImGuiThemes theme) {
@@ -160,6 +160,7 @@ public unsafe class FhImGuiHelper {
     /// Initialize values that require ImGui to be running. Called by Runtime.
     /// </summary>
     internal void init(FhImGuiThemes? theme = null) {
+        _init_fonts();
         _init_style(theme ?? FhImGuiThemes.CLASSIC_FF);
     }
 }
