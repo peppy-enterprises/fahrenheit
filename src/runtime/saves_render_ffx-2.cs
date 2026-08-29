1b6269e6a3d7679b91fe4b47fddeab51d08c063b
cxldalyy <callum.daly2504@gmail.com>
2026-08-29 09:37:23 +0100

    Fix `post_open` alignment again

--- src/runtime/saves_render_ffx-2.cs
+++ src/runtime/saves_render_ffx-2.cs
@@ -229,7 +229,7 @@ public sealed class FhSaveUiRendererX2 : FhSaveUiRenderer {
         _set_list.Clear();
         _set_list.AddRange(FhApi.Saves.get_sets());
 
-        _scrollable_sets.max = _set_list.Count;
+        _scrollable_sets .max = _set_list.Count;
         _scrollable_saves.max = is_saving
             ? FhApi.Saves.get_slots_used() + 1 // Add one for New Save Data button
             : FhApi.Saves.display_data.Count;
