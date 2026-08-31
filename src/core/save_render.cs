83e8741daad8a610f74e066c2a16919713f377b0
Dawn Veilwinter <DawnVeilwinter@protonmail.com>
2026-08-31 22:39:51 +0200

    Move 'Fahrenheit.Gui' to Core's global usings

--- src/core/save_render.cs
+++ src/core/save_render.cs
@@ -3,8 +3,6 @@
 // This file is part of Fahrenheit, © 2023-2026 The Fahrenheit contributors.
 // It is licensed to you under the GNU Lesser General Public License, version 3.0 or later. See COPYING, COPYING.LESSER.
 
-using Fahrenheit.Gui;
-
 namespace Fahrenheit;
 
 /// <summary>The base class for custom renderers of the save/load screen.</summary>
